7847e361dbfb354ea4ab08feacc61b2b04a44256
winiga <winiga@rocketmail.com>
2021-04-10 23:30:20 +0200

    correct links for https

---
+++ src/main/java/com/eii/jeeclassproject/jeeblog/utils/ParamConverter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eii.jeeclassproject.jeeblog.utils;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author winiga
+ */
+@FacesConverter("paramConverter")
+public class ParamConverter implements Converter<Object>{
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return value;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        String val = (String) value;
+        return val.replace("ç", "c");
+    }
+    
+}
